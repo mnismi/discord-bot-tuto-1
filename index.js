a7b2e2e74596d17a0ae1fe7421cb1254b9a5d43a
Nismi Mohamed <mh.mohamed.nismi@gmail.com>
2022-03-19 15:10:30 +0530

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('Logged in');
+});
+
+client.on('messageCreate', (message) => {
+  console.log(message.content);
+  message.react('😋');
+  message.react('📙');
+  message.reply('HEEEllooo');
+});
+
+client.on('typingStart', (props) => {
+  console.log('typing...');
+});
+
+client.login(process.env.TOKEN);
